a77511f56a6135dccdeda97ef0df28882b02c5b0
Vinicius Cogo <vinicius.cogo@voalle.com.br>
2023-10-02 14:35:09 -0300

    Descrição: Pequenas alterações.

--- src/components/ResumeItem/index.js
+++ src/components/ResumeItem/index.js
@@ -4,13 +4,19 @@ import * as C from "./styles";
 const ResumeItem = ({ title, Icon, value }) => {
 
   const isNegative = value < 0;
+
+  const monetaryValue = 'R$' + value.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  });
+
   return (
     <C.Container>
       <C.Header>
         <C.HeaderTitle>{title}</C.HeaderTitle>
         <Icon />
       </C.Header>
-        <C.Total negative={isNegative}>R$ {value}</C.Total>
+        <C.Total negative={isNegative}>{monetaryValue}</C.Total>
     </C.Container>
   );
 };
